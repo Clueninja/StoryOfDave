3e3731cfc7572af6190fe86e4970ecac86750e0b
Sam Brown <sbrown14@sheffield.ac.uk>
2023-03-02 13:15:40 +0000

    add I2C header and source

---
+++ I2C.h
@@ -0,0 +1,47 @@
+/* Microchip Technology Inc. and its subsidiaries.  You may use this software 
+ * and any derivatives exclusively with Microchip products. 
+ * 
+ * THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS".  NO WARRANTIES, WHETHER 
+ * EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED 
+ * WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A 
+ * PARTICULAR PURPOSE, OR ITS INTERACTION WITH MICROCHIP PRODUCTS, COMBINATION 
+ * WITH ANY OTHER PRODUCTS, OR USE IN ANY APPLICATION. 
+ *
+ * IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+ * INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+ * WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS 
+ * BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE.  TO THE 
+ * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS 
+ * IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF 
+ * ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *
+ * MICROCHIP PROVIDES THIS SOFTWARE CONDITIONALLY UPON YOUR ACCEPTANCE OF THESE 
+ * TERMS. 
+ */
+
+/* 
+ * File:   I2C.h
+ * Author: 
+ * Comments:
+ * Revision history: 
+ */
+
+// This is a guard condition so that contents of this file are not included
+// more than once.  
+#ifndef XC_HEADER_TEMPLATE_H
+#define	XC_HEADER_TEMPLATE_H
+
+#include <xc.h> // include processor files - each processor file is guarded.  
+
+
+void I2C_Initialise(void);      	//Initialise I2C
+void I2C_checkbus_free(void);   //Wait until I2C bus is free
+void I2C_Start(void);           	//Generate I2C start condition
+void I2C_RepeatedStart(void);  	 //Generate I2C Repeat start condition
+void I2C_Stop(void);           	 //Generate I2C stop condition
+void I2C_Write(unsigned char write);    //Generate I2C write condition
+unsigned char I2C_Read(void);   	//Generate I2C read condition
+
+
+#endif	/* XC_HEADER_TEMPLATE_H */
+
--- main.c
+++ main.c
@@ -4,8 +4,6 @@
  *
  * Created on 02 March 2023, 12:23
  */
-//vin was not here
-
 
 #include <xc.h>
 #include <stdio.h>
@@ -15,13 +13,8 @@
 #pragma config LVP = OFF   	//Low voltage programming disabled
 #pragma config PWRT = ON   	//Power up timer on
 
-void I2C_Initialise(void);      	//Initialise I2C
-void I2C_checkbus_free(void);   //Wait until I2C bus is free
-void I2C_Start(void);           	//Generate I2C start condition
-void I2C_RepeatedStart(void);  	 //Generate I2C Repeat start condition
-void I2C_Stop(void);           	 //Generate I2C stop condition
-void I2C_Write(unsigned char write);    //Generate I2C write condition
-unsigned char I2C_Read(void);   	//Generate I2C read condition
+#include "I2C.h"
+
 void main(void){
   unsigned char linesensor;    	 //Store raw data from sensor array
   TRISC = 0xFF;                	 //Set PORTC as inputs
@@ -44,52 +37,3 @@ void main(void){
 
 
 
-
-
-void I2C_Initialise(void)  	//Initialise I2C
-{
-  SSPCON1 = 0b00101000; 	//set to master mode, enable SDA and SCL pins
-  SSPCON2 = 0;         		 //reset control register 2
-  SSPADD = 0x63;       		 //set baud rate to 100KHz
-  SSPSTAT = 0;         		 //reset status register
-  }
-void I2C_checkbus_free(void)   	 //Wait until I2C bus is free
-{
-  while ((SSPSTAT & 0x04) || (SSPCON2 & 0x1F));	//wait until I2C bus is free
-}
-
-void I2C_Start(void)        //Generate I2C start condition
-{
-  I2C_checkbus_free(); 	 //Test to see I2C bus is free
-  SEN = 1;              	//Generate start condition,SSPCON2 bit 0 = 1
-}
-
-void I2C_RepeatedStart(void) 	//Generate I2C Repeat start condition
-{
-  I2C_checkbus_free();  		//Test to see I2C bus is free
-  RSEN = 1;             		//Generate repeat start, SSPCON2 bit1 = 1
-}
-
-void I2C_Stop(void) 		//Generate I2C stop condition
-{
-  I2C_checkbus_free();  		//Test to see I2C bus is free
-  PEN = 1;              		// Generate stop condition,SSPCON2 bit2 = 1
-}
-
-void I2C_Write(unsigned char write) 	//Write to slave
-{
-  I2C_checkbus_free();  		//check I2C bus is free
-  SSPBUF = write;       		//Send data to transmit buffer
-}
-
-unsigned char I2C_Read(void)    //Read from slave
-{
-  unsigned char temp;
-  I2C_checkbus_free(); 	 //Test to see I2C bus is free
-  RCEN = 1;            	 //enable receiver,SSPCON2 bit3 = 1
-  I2C_checkbus_free();  	//Test to see I2C bus is free
-  temp = SSPBUF;        	//Read slave
-  I2C_checkbus_free(); 	 //Test to see I2C bus is free
-  ACKEN = 1;           	 //Acknowledge
-  return temp;         	 //return sensor array data
-}
